ff3d707a9eab2fd39c550ff39ca80d2191a554cc
Rémy Frèrebeau <remy.frerebeau@systemathics.com>
2021-02-02 22:01:29 +0100

    install systemathics logo

--- src/index.ts
+++ src/index.ts
@@ -1,27 +1,40 @@
 import {
+  ILabShell,
   JupyterFrontEnd,
   JupyterFrontEndPlugin
 } from '@jupyterlab/application';
 
-import { IThemeManager } from '@jupyterlab/apputils';
+
+import { Widget } from '@lumino/widgets';
+import { LabIcon } from '@jupyterlab/ui-components';
+import stxSvgstr from '../style/systemathics-logo-squares.svg';
+
+export const stxIcon = new LabIcon({
+  name: 'barpkg:foo',
+  svgstr: stxSvgstr
+});
 
 /**
  * Initialization data for the ganymede-logo extension.
  */
 const extension: JupyterFrontEndPlugin<void> = {
   id: 'ganymede-logo',
-  requires: [IThemeManager],
   autoStart: true,
-  activate: (app: JupyterFrontEnd, manager: IThemeManager) => {
-    console.log('JupyterLab extension ganymede-logo is activated!');
-    const style = 'ganymede-logo/index.css';
-
-    manager.register({
-      name: 'ganymede-logo',
-      isLight: true,
-      load: () => manager.loadCSS(style),
-      unload: () => Promise.resolve(undefined)
+  requires: [ILabShell],
+  activate: (app: JupyterFrontEnd, shell: ILabShell) => {
+    console.log('ganymede-logo activated')
+    const logo = new Widget();
+    stxIcon.element({
+      container: logo.node,
+      elementPosition: 'center',
+      margin: '2px 2px 2px 8px',
+      height: 'auto',
+      width: '16px'
     });
+    logo.id = 'stx-MainLogo';
+    
+    shell.widgets('top').next().setHidden(true);
+    shell.add(logo, 'top', { rank: 0 });
   }
 };
 
